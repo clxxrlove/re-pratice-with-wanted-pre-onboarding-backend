52d2a53277f86faab3999ab0cef50f4d5e246b58
Jiyong Jung <hickerd@sch.ac.kr>
2024-04-02 00:29:49 +0900

    [Add] 프로젝트 Initialize

---
+++ src/test/java/xyz/jiyong/wantedpreonboarding/WantedPreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jiyong.wantedpreonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
