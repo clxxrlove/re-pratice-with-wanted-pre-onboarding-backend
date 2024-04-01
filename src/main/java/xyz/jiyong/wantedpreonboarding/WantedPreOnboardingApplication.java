271e3f050c1568a6ce55068f7d8654765679aa5a
Jiyong <hickerd@sch.ac.kr>
2024-04-02 00:34:09 +0900

    [Add] 프로젝트 Initialize

---
+++ src/main/java/xyz/jiyong/wantedpreonboarding/WantedPreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package xyz.jiyong.wantedpreonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreOnboardingApplication.class, args);
+	}
+
+}
