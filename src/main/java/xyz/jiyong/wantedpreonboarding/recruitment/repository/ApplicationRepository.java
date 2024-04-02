f7981bad033fe76e236a9cf57119410060b9c95d
Jiyong <hickerd@sch.ac.kr>
2024-04-03 01:04:20 +0900

    [Feat] Repository Annotation 추가

--- src/main/java/xyz/jiyong/wantedpreonboarding/recruitment/repository/ApplicationRepository.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/recruitment/repository/ApplicationRepository.java
@@ -1,7 +1,9 @@
 package xyz.jiyong.wantedpreonboarding.recruitment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import xyz.jiyong.wantedpreonboarding.recruitment.entity.Application;
 
+@Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 }
