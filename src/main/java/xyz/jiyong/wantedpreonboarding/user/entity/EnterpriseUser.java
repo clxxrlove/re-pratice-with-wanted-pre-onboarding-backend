478690d98362f7d31f7526decec8ca6b06973c85
Jiyong <hickerd@sch.ac.kr>
2024-04-02 12:34:06 +0900

    [Feat] 엔티티 간 관계 설정 -> 연관관계 주인 설정 및 mappedBy 설정

--- src/main/java/xyz/jiyong/wantedpreonboarding/user/entity/EnterpriseUser.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/user/entity/EnterpriseUser.java
@@ -2,8 +2,12 @@ package xyz.jiyong.wantedpreonboarding.user.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Getter;
+import xyz.jiyong.wantedpreonboarding.recruitment.entity.Recruitment;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,6 +16,9 @@ public class EnterpriseUser extends User {
 
     private String country;
     private String region;
+
+    @OneToMany(mappedBy = "enterpriseId")
+    private List<Recruitment> recruitments;
 
     @Builder
     protected EnterpriseUser(String email, String password, String name, String country, String region) {
--- src/main/java/xyz/jiyong/wantedpreonboarding/user/entity/PersonalUser.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/user/entity/PersonalUser.java
@@ -2,13 +2,20 @@ package xyz.jiyong.wantedpreonboarding.user.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Getter;
+import xyz.jiyong.wantedpreonboarding.recruitment.entity.Application;
+
+import java.util.List;
 
 @Entity
 @Getter
 @DiscriminatorValue("PERSONAL_USER")
 public class PersonalUser extends User {
+
+    @OneToMany(mappedBy = "account_id")
+    private List<Application> applications;
 
     @Builder
     protected PersonalUser(String email, String password, String name) {
