108e38a86d700858420c7a4803319246f7431497
Jiyong <hickerd@sch.ac.kr>
2024-04-04 02:52:01 +0900

    [Feat] 회원가입 시 사용되던 DTO 병합 및 Service에 반영

--- src/main/java/xyz/jiyong/wantedpreonboarding/user/dto/UserRegistrationDto.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/user/dto/UserRegistrationDto.java
@@ -1,19 +1,31 @@
 package xyz.jiyong.wantedpreonboarding.user.dto;
 
-import lombok.Builder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import xyz.jiyong.wantedpreonboarding.user.entity.EnterpriseUser;
 import xyz.jiyong.wantedpreonboarding.user.entity.PersonalUser;
-import xyz.jiyong.wantedpreonboarding.user.entity.User;
 
-@Builder
 public record UserRegistrationDto(
         String email,
         String password,
-        String name
+        String name,
+        String country,
+        String region,
+        String type
 ) {
-    public static PersonalUser toEntity(UserRegistrationDto userRegistrationDto) {
+    public static EnterpriseUser toEnterpriseEntity(UserRegistrationDto userRegistrationDto) {
+        return EnterpriseUser.builder()
+                .email(userRegistrationDto.email())
+                .password(new BCryptPasswordEncoder().encode(userRegistrationDto.password()))
+                .name(userRegistrationDto.name())
+                .country(userRegistrationDto.country())
+                .region(userRegistrationDto.region())
+                .build();
+    }
+
+    public static PersonalUser toPersonalEntity(UserRegistrationDto userRegistrationDto) {
         return PersonalUser.builder()
                 .email(userRegistrationDto.email())
-                .password(userRegistrationDto.password())
+                .password(new BCryptPasswordEncoder().encode(userRegistrationDto.password()))
                 .name(userRegistrationDto.name())
                 .build();
     }
--- src/main/java/xyz/jiyong/wantedpreonboarding/user/service/UserService.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/user/service/UserService.java
@@ -1,38 +1,58 @@
 package xyz.jiyong.wantedpreonboarding.user.service;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import xyz.jiyong.wantedpreonboarding.global.exception.CustomErrorCode;
 import xyz.jiyong.wantedpreonboarding.global.exception.CustomException;
-import xyz.jiyong.wantedpreonboarding.user.dto.EnterpriseUserRegistrationDto;
-import xyz.jiyong.wantedpreonboarding.user.dto.UserDto;
 import xyz.jiyong.wantedpreonboarding.user.dto.UserRegistrationDto;
-import xyz.jiyong.wantedpreonboarding.user.entity.PersonalUser;
+import xyz.jiyong.wantedpreonboarding.user.dto.UserDto;
+import xyz.jiyong.wantedpreonboarding.user.dto.UserLoginDto;
 import xyz.jiyong.wantedpreonboarding.user.entity.User;
 import xyz.jiyong.wantedpreonboarding.user.repository.EnterpriseUserRepository;
 import xyz.jiyong.wantedpreonboarding.user.repository.PersonalUserRepository;
 import xyz.jiyong.wantedpreonboarding.user.repository.UserRepository;
 
+@Service
 public class UserService {
 
     private final EnterpriseUserRepository enterpriseUserRepository;
     private final PersonalUserRepository personalUserRepository;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(EnterpriseUserRepository enterpriseUserRepository, PersonalUserRepository personalUserRepository, UserRepository userRepository) {
+    public UserService(EnterpriseUserRepository enterpriseUserRepository, PersonalUserRepository personalUserRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.enterpriseUserRepository = enterpriseUserRepository;
         this.personalUserRepository = personalUserRepository;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public void joinPersonalUser(UserRegistrationDto userRegistrationDto) {
-        personalUserRepository.save(UserRegistrationDto.toEntity(userRegistrationDto));
-    }
-
-    public void joinEnterpriseUser(EnterpriseUserRegistrationDto enterpriseUserRegistrationDto) {
-        enterpriseUserRepository.save(EnterpriseUserRegistrationDto.toEntity(enterpriseUserRegistrationDto));
+    public void join(UserRegistrationDto userRegistrationDto) {
+        try {
+            if (userRegistrationDto.type().equals("PERSONAL"))
+                personalUserRepository.save(UserRegistrationDto.toPersonalEntity(userRegistrationDto));
+            else if (userRegistrationDto.type().equals("ENTERPRISE"))
+                enterpriseUserRepository.save(UserRegistrationDto.toEnterpriseEntity(userRegistrationDto));
+            else
+                throw new CustomException(CustomErrorCode.INVALID_USER_TYPE);
+        } catch (DataIntegrityViolationException e) {
+            throw new CustomException(CustomErrorCode.DATA_INTEGRITY_VIOLATION);
+        }
     }
 
     public UserDto getUser(long id) {
         return UserDto.from(userRepository.findById(id)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND_ERROR)));
+    }
+
+    public UserDto login(UserLoginDto userLoginDto) {
+        User user = userRepository.findByEmail(userLoginDto.email())
+                .orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND_ERROR));
+
+        if (passwordEncoder.matches(userLoginDto.password(), user.getPassword())) {
+            return UserDto.from(user);
+        }
+        return null;
     }
 }
