8165404829367453ad209e69f326510c4cdaab44
Jiyong <hickerd@sch.ac.kr>
2024-04-06 00:01:40 +0900

    [Feat] Spring Security + JWT로 인증 기능 구현

--- src/main/java/xyz/jiyong/wantedpreonboarding/global/security/JwtTokenProvider.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/global/security/JwtTokenProvider.java
@@ -6,14 +6,20 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import xyz.jiyong.wantedpreonboarding.global.dto.JwtToken;
 import xyz.jiyong.wantedpreonboarding.global.exception.InvalidTokenException;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -25,14 +31,14 @@ public class JwtTokenProvider {
 
     public JwtTokenProvider(
             @Value("${jwt.secret}") String secretKey,
-            @Value("${jwt.token-validity-in-seconds") long tokenValidityInSeconds
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds
     ) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.tokenValidityInSeconds = tokenValidityInSeconds * 1000;
     }
 
-    public JwtToken generateToken(Authentication authentication, Long userId) {
+    public JwtToken generateToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
@@ -44,7 +50,6 @@ public class JwtTokenProvider {
         String accessToken = Jwts.builder()
                 .subject(authentication.getName())
                 .claim("auth", authorities)
-                .claim("userId", userId)
                 .expiration(tokenExpiresIn)
                 .signWith(key)
                 .compact();
@@ -74,26 +79,42 @@ public class JwtTokenProvider {
         }
     }
 
-    public Claims parseClaims(String accessToken) {
+    private Claims parseClaims(String accessToken) {
         try {
             return Jwts.parser()
                     .verifyWith(new SecretKeySpec(key.getEncoded(), key.getAlgorithm()))
                     .build()
-                    .parseUnsecuredClaims(accessToken)
-                    .getPayload();
+                    .parseClaimsJws(accessToken)
+                    .getBody();
         } catch (ExpiredJwtException e) {
             return e.getClaims();
         }
     }
 
-    public Long getUserIdFromClaims(String accessToken) {
-        Claims claims = parseClaims(accessToken);
-        Object userIdObject = claims.get("userId");
+    private String getJwtFromSecurityContext() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getCredentials() != null) {
+            return authentication.getCredentials().toString();
+        }
+        return null;
+    }
 
-        if (userIdObject instanceof Long userId) {
-            return userId;
-        } else {
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
             throw new InvalidTokenException("Invalid Token.");
         }
+
+        // 권한 없이 SimpleGrantedAuthority 사용시 InternalAuthenticationServiceException 발생
+//        Collection<? extends GrantedAuthority> authorities = Arrays.stream(
+//                claims.get("auth").toString().split(","))
+//                .map(SimpleGrantedAuthority::new)
+//                .toList();
+
+        Collection<? extends GrantedAuthority> authorities = Collections.emptyList();
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 }
---
+++ src/main/java/xyz/jiyong/wantedpreonboarding/global/security/SpringSecurity.java
@@ -0,0 +1,60 @@
+package xyz.jiyong.wantedpreonboarding.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurity {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/v1/user").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v2/user/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable)
+//                .exceptionHandling(authenticationManager -> authenticationManager
+//                        .authenticationEntryPoint()
+//                        .accessDeniedHandler())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
