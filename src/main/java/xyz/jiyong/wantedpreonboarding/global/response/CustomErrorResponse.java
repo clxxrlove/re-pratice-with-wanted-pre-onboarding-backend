889d0803e112b54b464d8c1c924bda225ce0d602
Jiyong <hickerd@sch.ac.kr>
2024-04-06 17:36:44 +0900

    [Chore] 메소드 이름 변경

--- src/main/java/xyz/jiyong/wantedpreonboarding/global/response/CustomErrorResponse.java
+++ src/main/java/xyz/jiyong/wantedpreonboarding/global/response/CustomErrorResponse.java
@@ -3,6 +3,7 @@ package xyz.jiyong.wantedpreonboarding.global.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import xyz.jiyong.wantedpreonboarding.global.exception.CustomErrorCode;
 
@@ -13,7 +14,7 @@ public class CustomErrorResponse {
 
     private String statusMessage;
 
-    public static ResponseEntity<CustomErrorResponse> toResponseEntity(CustomErrorCode e) {
+    public static ResponseEntity<CustomErrorResponse> toResponseEntityFromCustomCode(CustomErrorCode e) {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(CustomErrorResponse.builder()
